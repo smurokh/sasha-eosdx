eb25b302331fb7eda85f744af278b7ad4c13d02d
Sasha Murokh <smurokh50@stuy.edu>
2024-03-17 19:14:02 -0400

    update

--- test.py
+++ test.py
@@ -41,7 +41,7 @@ for item in ('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '
     count+=1
 
 
-from sklearn.decomposition import PCA
+##from sklearn.decomposition import PCA
 
 
 
